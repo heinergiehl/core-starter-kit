337573ff6ac8e7ed43d005187aa3f391ba4353e1
heiner <heinerDevelops@gmail.com>
2026-02-15 13:34:16 +0100

    perf: fix render-blocking fonts, robots.txt, and remaining PageSpeed issues
    - Make Google Fonts non-render-blocking via preload+onload in all layouts
    - Fix robots.txt sitemap URL to absolute (fixes 'Invalid sitemap URL')
    - Delete one-time convert-images.mjs utility

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -62,7 +62,8 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family={{ $fontFamilies }}&display=swap" rel="stylesheet">
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family={{ $fontFamilies }}&display=swap" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript><link href="https://fonts.googleapis.com/css2?family={{ $fontFamilies }}&display=swap" rel="stylesheet"></noscript>
 
         <style>
             :root {
